c422ae21ff3301959d7e167c44b257b9967f5024
inknsharps <nchen25@gmail.com>
2021-03-23 23:27:57 -0400

    add initial write file functionality

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log('The file has been saved!')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile("newreadme.md", "test");
+}
 
 // Function call to initialize app
 init();
